e835c762db4744a51b28107b25c0c4e2be670234
Ahmedabbas75 <ahmedmassa.757.pyf@gmail.com>
2024-01-12 19:04:55 +0200

    Add Problem #1704 Solution

--- Solutions/1704Determine_if_String_Halves.py
+++ Solutions/1704Determine_if_String_Halves.py
@@ -3,14 +3,15 @@ class Solution:
 
         vowels = {'a', 'e', 'i', 'o', 'u'}
         count, length = 0, len(s)
+        
         for index, value in enumerate(s.lower()):
             if index >= length // 2 and value in vowels:
                 count+= 1
             
             if index < length // 2 and value in vowels:
                 count-= 1
-                
-        return bool(count)
+
+            
                 
 S = Solution()
 print(S.halvesAreAlike(s = "book"))         
